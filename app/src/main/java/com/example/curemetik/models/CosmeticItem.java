d344dc22d42c218211c56530c7c0b3944e7baeb1
Kuritsina <pollypolly220@gmail.com>
2025-02-07 16:01:11 +0300

    feat: CosmeticItem

--- app/src/main/java/com/example/curemetik/models/CosmeticItem.java
+++ app/src/main/java/com/example/curemetik/models/CosmeticItem.java
@@ -4,15 +4,25 @@ public class CosmeticItem {
     private String name;
     private String description;
     private boolean isSelected;
+    private int rating;
 
     public CosmeticItem() {
         // Default constructor required for calls to DataSnapshot.getValue(CosmeticItem.class)
     }
 
-    public CosmeticItem(String name, String description, boolean isSelected) {
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public CosmeticItem(String name, String description, boolean isSelected, int rating) {
         this.name = name;
         this.description = description;
         this.isSelected = isSelected;
+        this.rating = rating;
     }
 
     public String getName() {
