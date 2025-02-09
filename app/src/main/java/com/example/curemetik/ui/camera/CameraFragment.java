97ea2cbd623dc2d7f26e6eefdcb22fdab0e2e4bd
Kuritsina <pollypolly220@gmail.com>
2025-02-09 17:40:16 +0300

    fix: camera

--- app/src/main/java/com/example/curemetik/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/curemetik/ui/camera/CameraFragment.java
@@ -1,9 +1,17 @@
 package com.example.curemetik.ui.camera;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +23,11 @@ import com.example.curemetik.databinding.FragmentCameraBinding;
 public class CameraFragment extends Fragment {
 
     private FragmentCameraBinding binding;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_PICK = 2;
+    private static final int REQUEST_PERMISSIONS = 100;
+    private ImageView imageView;
+    private Uri imageUri;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,10 +36,45 @@ public class CameraFragment extends Fragment {
 
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Button buttonGallery = binding.buttonGallery1;
+        Button buttonCamera = binding.buttonCamera1;
 
-        final TextView textView = binding.textCamera;
-        cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        buttonGallery.setOnClickListener(v -> openGallery());
+
+        // Обработка нажатия на кнопку "Сфотографировать"
+        buttonCamera.setOnClickListener(v -> openCamera());
+
+
         return root;
+    }
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_PICK);
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, "temp_image.jpg");
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        // values.put(MediaStore.Images.Media.DESCRIPTION, "Снимок продукта");
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+
+        ContentResolver resolver = requireContext().getContentResolver();
+        imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (intent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            // TODO Old Way
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+
+    private void findTextFromImage{
+
     }
 
     @Override
